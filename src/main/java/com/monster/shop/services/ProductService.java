31862bcca49868281af60155201d7313075f207b
May1704 <vergara.mayleris@gmail.com>
2025-06-19 11:09:03 +0200

    Create latest product methods and endpoints

--- src/main/java/com/monster/shop/services/ProductService.java
+++ src/main/java/com/monster/shop/services/ProductService.java
@@ -3,7 +3,10 @@ package com.monster.shop.services;
 import com.monster.shop.models.Product;
 import com.monster.shop.repositories.ProductRepository;
 import org.springframework.stereotype.Service;
-
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.http.HttpStatus;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -22,6 +25,26 @@ public class ProductService {
     //Get a product by ID
     public Product getProductsById(Long id) {
         return productRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Product not found with ID: " + id));
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Product not found with ID: " + id));
     }
+    //Create a product
+    public Product addProduct(Product newProduct) {
+        return productRepository.save(newProduct);
+    }
+    //Update a product
+    public Product update(Long id, Product updatedProduct) {
+        Product existing = getProductsById(id);
+        existing.setName(updatedProduct.getName());
+        existing.setPrice(updatedProduct.getPrice());
+        existing.setImageUrl(updatedProduct.getImageUrl());
+        existing.setRating(updatedProduct.getRating());
+        existing.setReviewCount(updatedProduct.getReviewCount());
+        existing.setFeatured(updatedProduct.isFeatured());
+        return productRepository.save(existing);
+    }
+    //Delete product
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
+
 }
